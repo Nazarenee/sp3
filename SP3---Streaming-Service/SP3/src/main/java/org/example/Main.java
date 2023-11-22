c30d6ad8da2819b156551636281424eaf464d103
Nazarenee <132462337+Nazarenee@users.noreply.github.com>
2023-11-22 08:48:14 +0100

    Add files via upload

---
+++ SP3---Streaming-Service/SP3/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+       FileIO io = new FileIO();
+       // Login log = new Login();
+    //    log.startmenu();
+    //   List<Media> moviedata = new ArrayList<>();
+    //    moviedata =   io.readSeriesData("C:\\Users\\chris\\IdeaProjects\\SP3---Streaming-Service\\SP3\\src\\main\\java\\org\\example\\100bedsteserier.txt");
+    //   System.out.println(moviedata.get(16).getCategory());
+    //
+    }
+
+}
---
+++ SP3---Streaming-Service/SP3/src/main/java/org/example/TextUI.java
@@ -0,0 +1,71 @@
+/*package main.java.org.example;
+import java.util.ArrayList;
+
+public class TextUI extends FileIO{
+
+    public void displayMessage(String msg){
+        System.out.println(msg);
+    }
+
+
+    public void displayMovies() {
+        for (String[] s : movieList) {
+            System.out.println(s);
+        }
+    }
+
+    public void displaySeries() {
+        for (String[] s : SeriesList) {
+            System.out.println(s);
+        }
+    }
+
+    public void displayWatchedMovie(User user){
+        User name = new User("","");
+        System.out.println(name.getUserName() + "," + " These are the movies that you have watched:");
+
+        for(String s : user){
+            System.out.println(s);
+        }
+
+        System.out.println("Would you like to watch a movie from the list? \n ** Y/N **");
+
+    }
+
+    public void displaySavedMovie(User user){
+        User name = new User("","");
+        System.out.println(name.getUserName()+ "," + " These are the movies you have saved for later viewing:");
+
+        for(String s : user){
+            System.out.println(s);
+        }
+
+        System.out.println("Would you like to watch a movie from the list? \n ** Y/N **");
+
+    }
+
+    public void displayWatchedSeries(User user){
+        User name = new User("","");
+        System.out.println(name.getUserName()+ "," + " This is a list of shows that you have watched:");
+
+        for(String s : user){
+            System.out.println(s);
+        }
+
+        System.out.println("Would you like to watch a show from the list? \n ** Y/N **");
+
+    }
+
+    public void displaySavedSeries(User user){
+        User name = new User("","");
+        System.out.println(name.getUserName()+ "," + " This is a list of shows you have saved for later viewing:");
+
+        for(String s : user){
+            System.out.println(s);
+        }
+
+        System.out.println("Would you like to watch a show from the list? \n ** Y/N **");
+
+    }
+
+}*/
